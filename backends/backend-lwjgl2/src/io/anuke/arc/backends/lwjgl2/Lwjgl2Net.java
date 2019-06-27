ca22b4d56fa78864c6fdc19cdb28063facc6c541
Anuken <arnukren@gmail.com>
2019-06-26 22:20:00 -0400

    no

--- backends/backend-lwjgl2/src/io/anuke/arc/backends/lwjgl2/Lwjgl2Net.java
+++ backends/backend-lwjgl2/src/io/anuke/arc/backends/lwjgl2/Lwjgl2Net.java
@@ -5,10 +5,6 @@ import io.anuke.arc.function.Consumer;
 import io.anuke.arc.util.NetJavaImpl;
 import org.lwjgl.Sys;
 
-/**
- * LWJGL implementation of the {@link Net} API, it could be reused in other Desktop backends since it doesn't depend on LWJGL.
- * @author acoppes
- */
 public class Lwjgl2Net implements Net{
     NetJavaImpl impl = new NetJavaImpl();
 
